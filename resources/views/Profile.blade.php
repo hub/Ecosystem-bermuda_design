220a2ac7070063e9df4ebe0e17f44b2ce3df7af4
serhiyosetskiy <serhiy.osetskiy@qbex.io>
2018-05-25 12:45:44 +0300

    update was Profile page

--- resources/views/Profile.blade.php
+++ resources/views/Profile.blade.php
@@ -6,46 +6,13 @@
         <img src="../dist/img/VectorSmartObjec-1.svg" alt="">
         <p>Add New Profile</p>
     </div>
-    <div class="box-profile">
-        <a href="#"><img class="img-edit" src="../dist/img/VectorSmartObjec-2.svg" alt=""></a>
-        <p class="profile-title">Very Large Company Name</p>
-        <p class="profile-hint">Ven Balance <span class="venAmount">10000</span></p>
-    </div>
-    <div class="box-profile">
-        <a href="#"><img class="img-edit" src="../dist/img/VectorSmartObjec-2.svg" alt=""></a>
-        <p class="profile-title">Very Large Company Name</p>
-        <p class="profile-hint">Ven Balance <span class="venAmount">10000</span></p>
-    </div>
-    <div class="box-profile">
-        <a href="#"><img class="img-edit" src="../dist/img/VectorSmartObjec-2.svg" alt=""></a>
-        <p class="profile-title">Very Large Company Name</p>
-        <p class="profile-hint">Ven Balance <span class="venAmount">10000</span></p>
-    </div>
-    <div class="box-profile">
-        <a href="#"><img class="img-edit" src="../dist/img/VectorSmartObjec-2.svg" alt=""></a>
-        <p class="profile-title">Very Large Company Name</p>
-        <p class="profile-hint">Ven Balance <span class="venAmount">10000</span></p>
-    </div>
-    <div class="box-profile">
-        <a href="#"><img class="img-edit" src="../dist/img/VectorSmartObjec-2.svg" alt=""></a>
-        <p class="profile-title">Very Large Company Name</p>
-        <p class="profile-hint">Ven Balance <span class="venAmount">10000</span></p>
-    </div>
-    <div class="box-profile">
-        <a href="#"><img class="img-edit" src="../dist/img/VectorSmartObjec-2.svg" alt=""></a>
-        <p class="profile-title">Very Large Company Name</p>
-        <p class="profile-hint">Ven Balance <span class="venAmount">10000</span></p>
-    </div>
-    <div class="box-profile">
-        <a href="#"><img class="img-edit" src="../dist/img/VectorSmartObjec-2.svg" alt=""></a>
-        <p class="profile-title">Very Large Company Name</p>
-        <p class="profile-hint">Ven Balance <span class="venAmount">10000</span></p>
-    </div>
-    <div class="box-profile">
-        <a href="#"><img class="img-edit" src="../dist/img/VectorSmartObjec-2.svg" alt=""></a>
-        <p class="profile-title">Very Large Company Name</p>
-        <p class="profile-hint">Ven Balance <span class="venAmount">10000</span></p>
-    </div>
+    @for ($i = 0; $i < 8; $i++)
+        <div class="box-profile">
+            <a href="#"><img class="img-edit" src="../dist/img/VectorSmartObjec-2.svg" alt=""></a>
+            <p class="profile-title">Very Large Company Name</p>
+            <p class="profile-hint">Ven Balance <span class="venAmount">10000</span></p>
+        </div>
+    @endfor
   </div>
 
 @endsection
